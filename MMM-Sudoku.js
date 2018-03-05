4ca58ca35dc8101319d33e0a272f36d254ce5ab9
Mykle <25211804+mykle1@users.noreply.github.com>
2018-03-04 22:33:14 -0500

    Add files via upload

--- MMM-Sudoku.js
+++ MMM-Sudoku.js
@@ -30,6 +30,17 @@ Module.register("MMM-Sudoku",{
 		iframe.src =  '//100widgets.com/js_data.php?id=162';
 		
 		return iframe;
-	}
+	},
+	
+	/////  Add this function to the modules you want to control with voice //////
+
+    notificationReceived: function(notification, payload) {
+        if (notification === 'HIDE_SUDOKU') {
+            this.hide(1000);
+        }  else if (notification === 'SHOW_SUDOKU') {
+            this.show(1000);
+        }
+            
+    },
 
 });
